c6c7b65ad602249a17b9f1ad9521c139c3415bd1
Lorenzo Fiamingo <lorenzofiamingo@MacBook-Pro-di-Lorenzo.local>
2019-01-24 01:00:13 +0100

    .

--- ioniCup/settings.py
+++ ioniCup/settings.py
@@ -33,8 +33,7 @@ INSTALLED_APPS = [
     'area_scorekeeper',
     'game_schedule',
     'game_referee',
-    'game_scoreboard',
-]
+    'game_scoreboard', ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -43,8 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 
 ROOT_URLCONF = 'ioniCup.urls'
 
@@ -58,11 +56,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+                'django.contrib.messages.context_processors.messages', ], }, }, ]
 
 WSGI_APPLICATION = 'ioniCup.wsgi.application'
 
@@ -73,28 +67,26 @@ WSGI_APPLICATION = 'ioniCup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
-]
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        # },
+    ]
 
 
 # Internationalization
