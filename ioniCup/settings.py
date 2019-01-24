ae5c8318951e640032d566efed7f5aeb3f2502ec
Lorenzo Fiamingo <l99fiamingo@gmail.com>
2019-01-24 04:25:13 +0100

    .

--- ioniCup/settings.py
+++ ioniCup/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = 'static'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
